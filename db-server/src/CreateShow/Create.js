4f2d41080d0a99af696b5c3d0f0cf48540361aec
austinStotts <austintstotts@gmail.com>
2022-07-31 20:13:54 -0500

    colors

--- db-server/src/CreateShow/Create.js
+++ db-server/src/CreateShow/Create.js
@@ -12,20 +12,63 @@ class Create extends Component {
             <div className="create-wrapper">
                 <p>Please Input Information to make new show</p>
                 <div className="inputs-wrapper">
-                    <input className="create-input not-blank" type={"text"} placeholder={"name"}></input>
-                    <input className="create-input not-blank" type={"text"} placeholder={"showid"}></input>
-                    <input className="create-input" type={"text"} placeholder={"url"}></input>
-                    <input className="create-input not-blank" type={"text"} placeholder={"promo"}></input>
-                    <input className="create-input not-blank" type={"text"} placeholder={"banner"}></input>
-                    <input className="create-input" type={"text"} placeholder={"description"}></input>
-                    <input className="create-input" type={"text"} placeholder={"year"}></input>
-                    <input className="create-input" type={"text"} placeholder={"studio"}></input>
-                    <input className="create-input not-blank" type={"text"} placeholder={"episodes"}></input>
-                    <input className="create-input" type={"text"} placeholder={"tags"}></input>
-                    <input className="create-input" type={"text"} placeholder={"score"}></input>
-                    <input className="create-input" type={"text"} placeholder={"status"}></input>
-                    <input className="create-input" type={"text"} placeholder={"native"}></input>
-                    <input className="create-input" type={"text"} placeholder={"season"}></input>
+                    <div className="pair-wrapper">
+                        <input className="create-input not-blank" type={"text"} placeholder={"name"}></input>
+                        <p className="input-description">
+                        the name of the show in english. Include all case sensitivities and symbols [Demon Slayer: Kimetsu no Yaiba]</p>
+                    </div>
+                    <div className="pair-wrapper">
+                        <input className="create-input not-blank" type={"text"} placeholder={"showid"}></input>
+                        <p className="input-description">The simplefied version of the show name. No spaces or special characters [demon_slayer_kimetsu_no_yaiba]</p>
+                    </div>
+                    <div className="pair-wrapper">
+                        <input className="create-input" type={"text"} placeholder={"url"}></input>
+                        <p className="input-description"></p>
+                    </div>
+                    <div className="pair-wrapper">
+                        <input className="create-input not-blank" type={"text"} placeholder={"promo"}></input>
+                        <p className="input-description"></p>
+                    </div>
+                    <div className="pair-wrapper">
+                        <input className="create-input not-blank" type={"text"} placeholder={"banner"}></input>
+                        <p className="input-description"></p>
+                    </div>
+                    <div className="pair-wrapper">
+                        <input className="create-input" type={"text"} placeholder={"description"}></input>
+                        <p className="input-description"></p>
+                    </div>
+                    <div className="pair-wrapper">
+                        <input className="create-input" type={"text"} placeholder={"year"}></input>
+                        <p className="input-description"></p>
+                    </div>
+                    <div className="pair-wrapper">
+                        <input className="create-input" type={"text"} placeholder={"studio"}></input>
+                        <p className="input-description"></p>
+                    </div>
+                    <div className="pair-wrapper">
+                        <input className="create-input not-blank" type={"text"} placeholder={"episodes"}></input>
+                        <p className="input-description"></p>
+                    </div>
+                    <div className="pair-wrapper">
+                        <input className="create-input" type={"text"} placeholder={"tags"}></input>
+                        <p className="input-description"></p>
+                    </div>
+                    <div className="pair-wrapper">
+                        <input className="create-input" type={"text"} placeholder={"score"}></input>
+                        <p className="input-description"></p>
+                    </div>
+                    <div className="pair-wrapper">
+                        <input className="create-input" type={"text"} placeholder={"status"}></input>
+                        <p className="input-description"></p>
+                    </div>
+                    <div className="pair-wrapper">
+                        <input className="create-input" type={"text"} placeholder={"native"}></input>
+                        <p className="input-description"></p>
+                    </div>
+                    <div className="pair-wrapper">
+                        <input className="create-input" type={"text"} placeholder={"season"}></input>
+                        <p className="input-description"></p>
+                    </div>
                 </div>
             </div>
         )
